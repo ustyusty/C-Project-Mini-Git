23b9d5b5aa7d97d1a1807ef00d55f1dce75cacde
Алексей Сысалов <alex@MacBook-Air--Usty.local>
2026-03-27 21:05:48 +0700

    fix(core) add, remove

--- include/minigit.h
+++ include/minigit.h
@@ -2,15 +2,31 @@
 #define MINIGIT_H
 
 #include <time.h>
-// структура хранения файла
+/* 
+Cтруктура хранения файла:
+
+`char` *name - название/путь
+`char` *content - содержимое
+`char` hash[41]
+
+`FileNode` *next
+*/
 typedef struct FileNode {
     char *name;
     char *content;
     char hash[41];
     struct FileNode *next;
 } FileNode;
+/*
+Cтруктура коммита:
 
-// структура коммита
+`char` hash[41]
+`char` *name - название
+`time_t` timestamp - время создания
+
+`FileNode` *files
+`Commit` *parent - предыдущий коммит
+*/
 typedef struct Commit{
     char hash[41];
     char *name;
--- src/main.c
+++ src/main.c
@@ -2,10 +2,10 @@
 #include "../include/minigit.h"
 int main(void){
     Commit *first_commit = init_repo();
-    Commit *add_file_1_commit = add_file(first_commit, "file1", "stuf1");
-    Commit *add_file_2_commit = add_file(add_file_1_commit, "file2", "stuf2");
-    Commit *remove_file_1_commit = remove_file(add_file_2_commit, "file3");
-    FileNode *cur = remove_file_1_commit->files;
+    Commit *commit = add_file(first_commit, "file1", "stuf1");
+    commit = add_file(commit, "file2", "stuf2");
+    commit = remove_file(commit, "file2");
+    FileNode *cur = commit->files;
     while (cur != NULL)
     {
         printf("%s\n", cur->name);
